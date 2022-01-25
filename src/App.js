875035a310485fd69f4b2988dd51681a40434223
mrcruz117 <mrcruz1989@gmail.com>
2022-01-25 10:11:11 -0500

    added routes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ import ForgotPassword from "./pages/ForgotPassword";
 function App() {
   return (
     <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<SignIn />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+        </Routes>
+      </Router>
       <h1>helloWorld</h1>
     </>
   );
