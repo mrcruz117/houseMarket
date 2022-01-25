e753f7c19e74099da83b77a0f0cdc2366dac9d12
mrcruz117 <mrcruz1989@gmail.com>
2022-01-25 15:45:25 -0500

    sign In complete

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,6 +1,14 @@
+import { useState, useEffect } from "react";
+import { getAuth } from "firebase/auth";
+
 function Profile() {
-    return <div>Profile</div>;
-  }
-  
-  export default Profile;
-  
+  const [user, setUser] = useState(null);
+  const auth = getAuth();
+  useEffect(() => {
+    setUser(auth.currentUser);
+  }, []);
+
+  return user ? <h1>{user.displayName}</h1> : "not logged in";
+}
+
+export default Profile;
