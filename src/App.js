544d0702630888ad2f7b4bbef36dac6d291607d7
mrcruz117 <mrcruz1989@gmail.com>
2022-01-25 10:05:20 -0500

    stubbed out pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Explore from "./pages/Explore";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword";
+
 function App() {
   return (
-    <div>
+    <>
       <h1>helloWorld</h1>
-    </div>
+    </>
   );
 }
 
