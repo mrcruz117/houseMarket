f3deb7e70bcf35a485ca5e41899a5ccf4f204928
mrcruz117 <mrcruz1989@gmail.com>
2022-01-26 16:38:30 -0500

    initialized state for CreateListing

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,6 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from "react-router-dom";
+import Spinner from "../components/Spinner";
 
 function CreateListing() {
+  const [geolocationEnabled, setGeolocationEnabled] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [formData, setFormData] = useState({
     type: "rent",
     name: "",
@@ -16,6 +21,29 @@ function CreateListing() {
     latitude: 0,
     longitude: 0,
   });
+
+  const auth = getAuth();
+  const navigate = useNavigate();
+  const isMounted = useRef(true);
+
+  useEffect(() => {
+    if (isMounted) {
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setFormData({
+            ...formData,
+            userRef: user.uid,
+          });
+        } else {
+          navigate("/sign-in");
+        }
+      });
+    }
+    if (loading) return <Spinner />;
+    return () => {
+      isMounted.current = false;
+    };
+  }, [isMounted]);
 
   return <div>Create</div>;
 }
