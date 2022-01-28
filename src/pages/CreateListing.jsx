0c57b55a75071949b0693851b4a87a3517fba49c
mrcruz117 <mrcruz1989@gmail.com>
2022-01-28 15:47:14 -0500

    listing object as expected

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -83,11 +83,23 @@ function CreateListing() {
 
     if (geolocationEnabled) {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyCMo-9SUNKKeUbVHulEOIcPJEQnZStOEEs`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCIDE_API_KEY}`
       );
 
       const data = await response.json();
-      console.log(data);
+
+      geolocation.lat = data.results[0]?.geometry.location.lat ?? 0;
+      geolocation.lng = data.results[0]?.geometry.location.lng ?? 0;
+      location =
+        data.status === "ZERO_RESULTS"
+          ? undefined
+          : data.results[0]?.formatted_address;
+
+      if (location === undefined || location.includes("undefined")) {
+        setLoading(false);
+        toast.error("Please enter a valid address");
+        return;
+      }
     } else {
       geolocation.lat = latitude;
       geolocation.lng = longitude;
